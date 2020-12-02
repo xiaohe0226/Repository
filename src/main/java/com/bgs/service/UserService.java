d4d49721be9ee8cc309bd485b6a103d38f7c5738
MSI-NB <256177171@qq.com>
2020-12-02 13:47:33 +0800

    test12

---
+++ src/main/java/com/bgs/service/UserService.java
@@ -0,0 +1,28 @@
+package com.bgs.service;
+
+import com.bgs.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import sun.dc.pr.PRError;
+
+import javax.xml.ws.Action;
+import java.util.List;
+
+public class UserService<UserDAO> {
+
+
+@Autowired
+    private UserDAO userDao;
+
+    public List<User> queryUserList() {
+        return null;
+
+    }
+
+    {
+
+
+    }
+
+
+
+}
